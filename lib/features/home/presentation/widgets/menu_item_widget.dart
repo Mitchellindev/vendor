4178f0e6b06651be37901d025b329ae491d8737b
Okwor Chukwuemeka Nnajiofor <88171212+thenaijadev@users.noreply.github.com>
2024-04-21 03:16:56 +0100

    Feat-Side Menu Drawer

--- lib/features/home/presentation/widgets/menu_item_widget.dart
+++ lib/features/home/presentation/widgets/menu_item_widget.dart
@@ -6,30 +6,37 @@ class MenuItemWidget extends StatelessWidget {
       {super.key,
       required this.iconPath,
       required this.label,
-      this.imageWidth = 24});
+      this.imageWidth = 24,
+      this.color = Colors.white,
+      required this.onTap});
   final String iconPath;
   final String label;
   final double imageWidth;
+  final VoidCallback onTap;
+  final Color color;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30.0, vertical: 20),
-      child: Row(
-        children: [
-          Image.asset(
-            iconPath,
-            width: imageWidth,
-          ),
-          const SizedBox(
-            width: 15,
-          ),
-          TextWidget(
-            text: label,
-            fontSize: 18,
-            fontWeight: FontWeight.w400,
-            color: Colors.white,
-          ),
-        ],
+      child: GestureDetector(
+        onTap: onTap,
+        child: Row(
+          children: [
+            Image.asset(
+              iconPath,
+              width: imageWidth,
+            ),
+            const SizedBox(
+              width: 15,
+            ),
+            TextWidget(
+              text: label,
+              fontSize: 18,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/features/home/presentation/widgets/menu_widget.dart
+++ lib/features/home/presentation/widgets/menu_widget.dart
@@ -1,5 +1,6 @@
 import 'package:broadcaadvendor/features/home/presentation/widgets/menu_item_widget.dart';
 import 'package:broadcaadvendor/features/home/presentation/widgets/notification_menu_item.dart';
+import 'package:broadcaadvendor/features/home/presentation/widgets/privacy_policy_widget.dart';
 import 'package:flutter/material.dart';
 
 class MenuWidget extends StatefulWidget {
@@ -28,20 +29,45 @@ class _MenuWidgetState extends State<MenuWidget> {
             height: 75,
           ),
           const NotificationMenuItem(),
-          const MenuItemWidget(
-              iconPath: "assets/images/chat_menu_item.png", label: "My Chat"),
-          const MenuItemWidget(
+          MenuItemWidget(
+              onTap: () {},
+              iconPath: "assets/images/chat_menu_item.png",
+              label: "My Chat"),
+          MenuItemWidget(
+              onTap: () {},
               iconPath: "assets/images/profile.png",
               imageWidth: 22,
               label: "My Profile"),
-          const MenuItemWidget(
+          MenuItemWidget(
+              onTap: () {},
               iconPath: "assets/images/cart.png",
               imageWidth: 28,
               label: "My Orders"),
-          const MenuItemWidget(
-              iconPath: "assets/images/clock.png", label: "Pending Items"),
-          const MenuItemWidget(
-              iconPath: "assets/images/chart.png", label: "My Store Analytics")
+          MenuItemWidget(
+              onTap: () {},
+              iconPath: "assets/images/clock.png",
+              label: "Pending Items"),
+          MenuItemWidget(
+              onTap: () {},
+              iconPath: "assets/images/chart.png",
+              label: "My Store Analytics"),
+          MenuItemWidget(
+            onTap: () {},
+            iconPath: "assets/images/logout.png",
+            label: "Logout",
+            color: const Color(0xffFF0000),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          Image.asset(
+            "assets/images/upgrade_to_premium.png",
+            width: 220,
+          ),
+          const SizedBox(
+            height: 70,
+          ),
+          const PrivacyPolicyWidget(),
         ],
       ),
     );
