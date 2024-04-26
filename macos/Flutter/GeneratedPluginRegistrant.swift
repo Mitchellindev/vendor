8aff27c40a46fe43fae6c68951b2d5d5acadbe22
ogagaCodes <ogagaadjaro@gmail.com>
2024-04-26 23:08:56 +0100

    Fix conflicts

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,13 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< HEAD
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-=======
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
->>>>>>> 48259b50136900464da49a7ec9c0bd5dd18c4e28
 }
